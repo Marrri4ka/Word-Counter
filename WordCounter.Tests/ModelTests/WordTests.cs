16ea61f92f8fef153cb3831580b5241d924c5d2a
Mariia <mariiapopovych@gmail.com>
2019-04-28 09:55:38 -0700

    add tests

--- WordCounter.Tests/ModelTests/WordTests.cs
+++ WordCounter.Tests/ModelTests/WordTests.cs
@@ -10,27 +10,76 @@ namespace WordCount.Tests
   public class WordTest
   {
     [TestMethod]
-    public void CountWords_Test()
+    public void CountWords_Test_TwoSameWords()
     {
       // Arrange
-      Word newWord = new Word("cat","catthedral Cat dog");
+      Word newWord = new Word("cat","cat cat");
 
       //Act
       int result = newWord.CountWords();
       //Assert
+      Assert.AreEqual(result, 2);
+    }
+
+
+    [TestMethod]
+    public void CountWords_Test_UpperCaseWord()
+    {
+      Word newWord = new Word("cat","Cat");
+      int result = newWord.CountWords();
       Assert.AreEqual(result, 1);
     }
+
+    [TestMethod]
+    public void CountWords_Test_DifferentCaseWord()
+    {
+      Word newWord = new Word("cat","cAt");
+      int result = newWord.CountWords();
+      Assert.AreEqual(result, 1);
+    }
+
+
+    [TestMethod]
+    public void CountWords_Test_Fullword_Match()
+    {
+      Word newWord = new Word("cat","Cathedral");
+      int result = newWord.CountWords();
+      Assert.AreEqual(result, 0);
+    }
+
+
+    [TestMethod]
+    public void CountWords_Test_NoSameWords()
+    {
+      Word newWord = new Word("cat","dog");
+      int result = newWord.CountWords();
+      Assert.AreEqual(result, 0);
+    }
+
+    [TestMethod]
+    public void CountWords_Test_NoWordInput()
+    {
+      Word newWord = new Word(" ","dog");
+      int result = newWord.CountWords();
+      Assert.AreEqual(result, 0);
+    }
+
+
+    [TestMethod]
+    public void CountWords_Test_NoSentenceInput()
+    {
+      Word newWord = new Word("cat","");
+      int result = newWord.CountWords();
+      Assert.AreEqual(result, 0);
+    }
+
 
     [TestMethod]
 
     public void IsValidInput_Test()
     {
-      //Arrange
       Word newWord = new Word("6", "cat dog");
-
-      // Act
       string result = newWord.IsValidInput();
-      // Assert
       Assert.AreEqual(result,  "Only letters please");
     }
   }
